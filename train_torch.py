c7f6e9fac7cfa2c316391cbe11083acae99527a8
CryoCardiogram <menalewa@live.com>
2019-03-08 03:02:49 +0100

    refactor argparse

--- train_torch.py
+++ train_torch.py
@@ -11,6 +11,25 @@ import torch.optim as optim
 from torch.autograd import Variable
 from model import STRNN
 
+# Argument Parser
+# ==================================================
+aparse = configargparse.ArgumentParser(description='train STRNNN model')
+
+aparse.add_argument('-d', '--dim', default=13, type=int, help='Dimensionality of the hidden layers')
+aparse.add_argument('-w', '--ww', default=360, type=int, help='width of the time window in minutes') #TODO generate preprocessed data
+aparse.add_argument('-r', '--reg_lambda', default=0.1, type=float, help='weight decay')
+aparse.add_argument('-b', '--batch_size', default=2, type=int, help='size of each training batch ')
+aparse.add_argument('-n', '--num_epochs', default=20, type=int, help='number of training iterations')
+aparse.add_argument('-l', '--learning_rate', default=0.001, type=float)
+aparse.add_argument('-m', '--momentum', default=0.9, type=float)
+aparse.add_argument('-e', '--evaluate_every', default=1, type=int, help="number of training iteration between two performance evaluation")
+aparse.add_argument('--train_file', default="./prepro_train_50.txt", help='path to preprocessed train set')
+aparse.add_argument('--valid_file', default='./prepro_valid_50.txt', help='path to preprocessed validation set')
+aparse.add_argument('--test_file', default="./prepro_test_50.txt", help='path to preprocessed test set')
+aparse.add_argument('-p', '--model_path', default='./model.pt', help='path to save model')
+aparse.add_argument('-s', '--save_every', default=2, type=int, help='number of training iteration between checkpoint creation')
+
+args, unknown = aparse.parse_known_args()
 
 # Parameters
 # ==================================================
@@ -18,47 +37,66 @@ ftype = torch.FloatTensor
 ltype = torch.LongTensor
 
 # Data loading params
-train_file = "./prepro_train_50.txt"
-valid_file = "./prepro_valid_50.txt"
-test_file = "./prepro_test_50.txt"
-
-# Model Hyperparameters
-dim = 13    # dimensionality
-ww = 720  # winodw width (6h)
-up_time = 560632.0  # min
-lw_time = 0.
-up_dist = 457.335   # km
-lw_dist = 0.
-reg_lambda = 0.1
-
-# Training Parameters
-batch_size = 2
-num_epochs = 30
-learning_rate = 0.001
-momentum = 0.9
-evaluate_every = 1
-h_0 = Variable(torch.randn(dim, 1), requires_grad=False).type(ftype)
-
-#user_cnt = 32899 #50 #107092#0
-#loc_cnt = 1115406 #50 #1280969#0
-#user_cnt = 42242 #30
-#loc_cnt = 1164559 #30
+# train_file = './prepro_train_50.txt'
+# valid_file = "./prepro_valid_50.txt"
+# test_file = "./prepro_test_50.txt"
 
 # Data Preparation
 # ===========================================================
 # Load data
 print("Loading data...")
-train_user, train_td, train_ld, train_loc, train_dst = data_loader.treat_prepro(train_file, step=1)
-valid_user, valid_td, valid_ld, valid_loc, valid_dst = data_loader.treat_prepro(valid_file, step=2)
-test_user, test_td, test_ld, test_loc, test_dst = data_loader.treat_prepro(test_file, step=3)
+train_user, train_td, train_ld, train_loc, train_dst = data_loader.treat_prepro(args.train_file, step=1)
+valid_user, valid_td, valid_ld, valid_loc, valid_dst = data_loader.treat_prepro(args.valid_file, step=2)
+test_user, test_td, test_ld, test_loc, test_dst = data_loader.treat_prepro(args.test_file, step=3)
 
+# Model Hyper parameters
+# dim = 13    # dimensionality
+# ww = 720  # winodw width (6h)
+
+
+def bound(interval_list, fun):
+    m = []
+    for l in interval_list:
+        for a in l:
+            m.append(fun(a))
+    return fun(m)
+
+
+up_time = bound(train_td, max)
+lw_time = bound(train_td, min)
+up_dist = bound(train_ld, max)
+lw_dist = bound(train_ld, min)
 user_cnt = len(train_user) + len(valid_user) + len(test_user)
 loc_cnt = sum(len(i) for i in train_loc) + sum(len(i) for i in valid_loc) + sum(len(i) for i in test_loc)
 
+# up_time = 560632.0  # min
+# lw_time = 0.
+# up_dist = 457.335   # km
+# lw_dist = 0.
+# reg_lambda = 0.1
+
+# Training Parameters
+# batch_size = 2
+# num_epochs = 30
+# learning_rate = 0.001
+# momentum = 0.9
+# evaluate_every = 1
+h_0 = Variable(torch.randn(args.dim, 1), requires_grad=False).type(ftype)
+
+# user_cnt = 32899 #50 #107092#0
+# loc_cnt = 1115406 #50 #1280969#0
+# user_cnt = 42242 #30
+# loc_cnt = 1164559 #30
+
+
 print("User/Location: {:d}/{:d}".format(user_cnt, loc_cnt))
+print("max/min time interval: {} / {} min".format(up_time, lw_time))
+print("max/min geo distance: {} / {} km".format(up_dist, lw_dist))
 print("==================================================================================")
 
 ###############################################################################################
+
+
 def parameters(strnn_model):
     params = []
     for model in [strnn_model]:
@@ -76,10 +114,10 @@ def print_score(batches, step):
     recall10000 = 0.
     iter_cnt = 0
 
-    for batch in tqdm.tqdm(batches, desc="validation"):
+    for batch in tqdm.tqdm(batches, desc="validation" if step == 2 else 'Test'):
         batch_user, batch_td, batch_ld, batch_loc, batch_dst = batch
-        if len(batch_loc) < 3:
-            continue
+        #if len(batch_loc) < 3:
+        #    continue
         iter_cnt += 1
         batch_o, target = run(batch_user, batch_td, batch_ld, batch_loc, batch_dst, step=step)
 
@@ -140,6 +178,16 @@ def run(user, td, ld, loc, dst, step):
 
 def train(model):
 
+    def create_checkpoint(model, total_loss, loss_per_epoch):
+        print("saving progress...")
+        torch.save({
+            'epoch': i,
+            'model_state_dict': model.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+            'current_loss': total_loss,
+            'loss_per_epoch': loss_per_epoch
+        }, "checkpoint.tar")
+
     loss_per_epoch = {}
 
     first_pass = False
@@ -154,7 +202,7 @@ def train(model):
     except FileNotFoundError:
         checkpoint = None
 
-    for i in range(num_epochs if checkpoint is None else num_epochs - checkpoint['epoch']):
+    for i in range(args.num_epochs if checkpoint is None else args.num_epochs - checkpoint['epoch']):
         # Training
         total_loss = 0.
 
@@ -169,45 +217,43 @@ def train(model):
                 # inner_batches = data_loader.inner_iter(train_batch, batch_size)
                 # for k, inner_batch in inner_batches:
                 batch_user, batch_td, batch_ld, batch_loc, batch_dst = train_batch  # inner_batch)
-                if len(batch_loc) < 3:
-                    continue
+                #if len(batch_loc) < 3:
+                #    continue
                 loss = run(batch_user, batch_td, batch_ld, batch_loc, batch_dst, step=1)
                 total_loss += loss
                 # if (j+1) % 2000 == 0:
                 #    print("batch #{:d}: ".format(j+1)), "batch_loss :", total_loss/j, datetime.datetime.now()
             # Evaluation
-            if (i + 1) % evaluate_every == 0:
+            if (i + 1) % args.evaluate_every == 0:
                 print("==================================================================================")
                 # print("Evaluation at epoch #{:d}: ".format(i+1)), total_loss/j, datetime.datetime.now()
                 valid_batches = list(zip(valid_user, valid_td, valid_ld, valid_loc, valid_dst))
                 print_score(valid_batches, step=2)
 
             loss_per_epoch[i] = total_loss
+
+            if i % args.save_every == 0:
+                create_checkpoint(model, total_loss, loss_per_epoch)
+
         except KeyboardInterrupt:
-            print("saving progress...")
-            torch.save({
-                'epoch': i,
-                'model_state_dict': model.state_dict(),
-                'optimizer_state_dict': optimizer.state_dict(),
-                'current_loss': total_loss,
-                'loss_per_epoch': loss_per_epoch
-            }, "checkpoint.tar")
+            create_checkpoint(model, total_loss, loss_per_epoch)
             sys.exit()
 
+    # Save trained model
+    torch.save(model.state_dict(), args.model_path)
 
 ###############################################################################################
 
 
-if __name__ == '__main__':
-    strnn_model = STRNN(dim, loc_cnt, user_cnt)
-    optimizer = optim.SGD(parameters(strnn_model), lr=learning_rate, momentum=momentum, weight_decay=reg_lambda)
-    # num_processes = torch.get_num_threads()
-    train(strnn_model)
+strnn_model = STRNN(args.dim, loc_cnt, user_cnt)
+optimizer = optim.SGD(parameters(strnn_model), lr=args.learning_rate, momentum=args.momentum, weight_decay=args.reg_lambda)
+# num_processes = torch.get_num_threads()
+train(strnn_model)
 
 
-    # Testing
-    print("Training End..")
-    print("==================================================================================")
-    print("Test: ")
-    test_batches = list(zip(test_user, test_td, test_ld, test_loc, test_dst))
-    print_score(test_batches, step=3)
+# Testing
+print("Training End..")
+print("==================================================================================")
+print("Test: ")
+test_batches = list(zip(test_user, test_td, test_ld, test_loc, test_dst))
+print_score(test_batches, step=3)
